f4860ba18c7ab2ab315880e60077c7916a7d31fe
uchenna23 <uchennanwagbara23@gmail.com>
2025-08-03 20:39:36 -0700

    initial-commit

---
+++ src/main/java/com/uchenna-nwagbara/JustHoopin/App.java
@@ -0,0 +1,13 @@
+package com.uchenna-nwagbara.JustHoopin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
